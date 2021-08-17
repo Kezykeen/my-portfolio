d397522daa449c29edcc3b8752bc0096d7ef4bd6
kezykeen <okorokingsley250@gmail.com>
2021-08-17 12:22:56 +0100

    Added animated text

---
+++ src/components/AnimatedText/AnimatedText.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Typing from "../Typing";
+import { TextTitle } from "./AnimatedTextStyles";
+
+const AnimatedText = () => {
+  return (
+    <TextTitle main>
+      <Typing />
+    </TextTitle>
+  );
+};
+
+export default AnimatedText;
