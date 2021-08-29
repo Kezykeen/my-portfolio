685a1f905cd148e3a9c61d78b8d7554f6d1f4e92
kezykeen <okorokingsley250@gmail.com>
2021-08-29 18:55:10 +0100

    updated data on projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,41 +3,41 @@ export const projects = [
     title: "LinkedIn-Clone",
     description:
       "This is a linkedIn clone built using React and Firebase for storage and deployment which simulates linkedIn processes.",
-    image: "/images/1.png",
+    image: "/images/linkedIn-clone.png",
     tags: ["React", "Firebase", "Material UI", "Redux Toolkit"],
     source: "https://github.com/Kezykeen/LinkedIn-Clone",
     visit: "https://linkedin-clone-d9c75.web.app/",
     id: 0,
   },
   {
+    title: "Gmail-Clone",
+    description:
+      "This is a linkedIn clone built using React and Firebase for storage and deployment which simulates linkedIn processes.",
+    image: "/images/gmail-clone.png",
+    tags: ["React", "Firebase", "Material UI", "Redux Toolkit"],
+    source: "https://github.com/Kezykeen/Gmail-Clone",
+    visit: "https://kezy-gmail-clone.vercel.app/",
+    id: 3,
+  },
+  {
+    title: "Moshify",
+    description:
+      "A single paged mobile responsive dummy platform for hosting websites. ",
+    image: "/images/moshify.png",
+    tags: ["HTML5", "CSS3", "JavaScript"],
+    source: "https://github.com/Kezykeen/Moshify",
+    visit: "https://prohosting.netlify.app/",
+    id: 2,
+  },
+  {
     title: "FYLO",
     description:
-      "This is a single page design using html5, css3 and bootstrap. This was a test from frontend mentor.",
-    image: "/images/2.png",
+      "Fylo is a single paged mobile responsive website designed using HTML5, CSS3 and Bootstrap. This was a practice site from frontend mentor.",
+    image: "/images/fylo.png",
     tags: ["HTML5", "CSS3"],
     source: "https://github.com/Kezykeen/Fylo",
     visit: "https://fylo-kezyapps.netlify.app/",
     id: 1,
-  },
-  {
-    title: "WebRTC App",
-    description:
-      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-    image: "/images/3.jpg",
-    tags: ["React", "WebRTC"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 2,
-  },
-  {
-    title: "Unichat",
-    description:
-      "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: "/images/4.jpg",
-    tags: ["React", "ChatEngine", "Firebase"],
-    source: "https://google.com",
-    visit: "https://google.com",
-    id: 3,
   },
 ];
 
