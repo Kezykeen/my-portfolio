e4bb0b5c2c5ac0be3834e1b1b570adef296a0b8d
kezykeen <okorokingsley250@gmail.com>
2021-08-29 06:54:20 +0100

    Added accomplishment component

---
+++ src/components/Accomplishments/Accomplishments.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { Box, Boxes, BoxNum, BoxText } from "./AccomplishmentsStyles";
+import { data } from "../../constants/constants";
+
+const Acomplishments = () => (
+  <Section>
+    <SectionDivider divider />
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}+`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
+);
+
+export default Acomplishments;
